bc1c2f20268298f68520a2a07e0e3b704449fbe9
Valentin Lab <valentin.lab@kalysto.org>
2023-05-05 13:58:55 +0200

    chg: dev: move ``makePasswordChecker`` to ``lokapi``

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import Jsc3lAbstract from '@com-chain/jsc3l'
 
 import { t, e } from '@lokavaluto/lokapi'
 import { mux } from '@lokavaluto/lokapi/build/generator'
+import { makePasswordChecker } from '@lokavaluto/lokapi/build/backend/utils'
 import { BackendAbstract } from '@lokavaluto/lokapi/build/backend'
 
 import { ComchainAccount } from './account'
@@ -13,40 +14,6 @@ import { ComchainCreditRequest } from './creditRequest'
 interface IJsonDataWithAddress extends t.JsonData {
     address: string
 }
-
-
-/** Password checking utilities */
-
-const PASSWORD_REGEX = {
-    tooShort: (size: number) => RegExp(`.{${size},}`),
-    noUpperCase: /[A-Z]/,
-    noLowerCase: /[a-z]/,
-    noDigit: /[0-9]/,
-    noSymbol: /[^A-Za-z0-9]/,
-
-}
-
-
-function makePasswordChecker (
-    checks: Array<String>
-): (password: string) => Array<string> {
-    return (password) => {
-        const issues = []
-        checks.forEach((checkStr) => {
-            const [checkId, argsStr] = checkStr.split(':')
-            const args = (argsStr || '').split(',')
-            let check = PASSWORD_REGEX[checkId]
-            if (!check) {
-                throw new Error(`Invalid check identifier ${checkId}`)
-            }
-            check = check instanceof RegExp ? check : check.apply(null, [args])
-
-            if (!check.test(password)) issues.push(checkStr)
-        })
-        return issues
-    }
-}
-
 
 export default abstract class ComchainBackendAbstract extends BackendAbstract {
 
